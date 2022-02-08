5750e205deba29f9683b539320dddc2cad96e017
santhaprakash <Santhaprakashm.19ece@kongu.edu>
2022-02-08 13:24:41 +0530

    san

--- src/PhonePage/Feed.js
+++ src/PhonePage/Feed.js
@@ -9,7 +9,7 @@ import { useHistory } from 'react-router-dom';
 function Feed() {
   const history=useHistory()
   const homeRoute=()=>{
-    history.push('/gallery')
+    history.push('/addpost')
   }
   return (
   <>
